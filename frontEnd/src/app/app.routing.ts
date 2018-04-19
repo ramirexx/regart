1bcfadf73039985f5fa2eef51af4c92ec548ab59
ramirex0 <ramirex0>
2018-04-18 23:51:54 -0400

    adicion de vista login

--- frontEnd/src/app/app.routing.ts
+++ frontEnd/src/app/app.routing.ts
@@ -10,7 +10,8 @@ import { Auth } from './auth/index';
 
 export const appRoutes: Routes = [
     
-    { path: '', component: HomeComponent, canActivate: [Auth] },
+    //{ path: '', component: HomeComponent, canActivate: [Auth] },
+    { path: '', component: HomeComponent},
     { path: 'login', component: LoginComponent },
     { path: 'tipo-registro', component: RegistroComponent },
     { path: 'registro-individual', component: IndividualComponent },
